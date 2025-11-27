0632aa0339d54c43decdad5832b54bfdd619c9aa
sh5914 <e6395316@gmail.com>
2025-11-27 15:19:06 +0900

    Create test1.c

---
+++ test1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+  //int i = 0;
+  //char BUF[256];
+  
+  while(1){
+    printf("hello");
+    //scanf("%s", BUF);
+    //printf("You input[%d]:  %s\n", i++, BUF);
+    }
+
+  return 0;
+  }
+
+
